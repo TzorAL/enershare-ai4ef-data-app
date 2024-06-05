163670fc578c4e3391e62e3580ea32d49d8007c7
TzorAL <tzortzisalex99@gmail.com>
2024-06-05 19:51:30 +0300

    Added .env and modified dockerfiles/api

--- api.py
+++ api.py
@@ -14,12 +14,14 @@ from sqlalchemy.schema import CreateSchema
 from sqlalchemy.orm import sessionmaker, Session
 from fastapi import  Depends
 
+from typing import Optional
+import yaml
 #  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Globals ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 tags_metadata = [
-    {"name": "Get table dataframe", "description": "get table as pandas dataframe"},
-    {"name": "Get tables list", "description": "get list of tables in db"},
-    {"name": "Get table dataframe", "description": "get table as pandas dataframe"},
+    {"name": "Schemas", "description": "Schema-related endpoints"},
+    {"name": "Tables", "description": "Table-related endpoints"},
+    {"name": "Data", "description": "Data-related endpoints"},
 ]
 
 app = FastAPI(
@@ -40,10 +42,6 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
-# API endpoint
-# url = 'https://enershare.epu.ntua.gr/provider-data-app/openapi/0.5/'    # https://<baseurl>/<data-app-path>/openapi/<beckend-service-version>/
-# endpoint = 'efcomp'                                                     # API endpoint 
 
 # Load variables from .env file
 load_dotenv()
@@ -81,6 +79,19 @@ def get_metadata(schema='public'):
     metadata.reflect(bind=engine, schema=schema)    
     return metadata
 
+def create_dataframe(data):
+    if isinstance(data, dict) and all(isinstance(value, (int, float, str, bool)) for value in data.values()):
+        data = [data]  # Wrap in a list to treat it as a single row
+    return pd.DataFrame(data)
+
+def store_to_db(data, table_name, pilot):
+    try:
+        # Convert DataFrame to SQL table
+        create_dataframe(data).to_sql(table_name, engine, schema=pilot, if_exists='replace', index=False)
+        print("Data has been successfully stored in the database.")
+    except Exception as e:
+        print("An error occurred while storing data in the database:", e)
+
 # Function to create schema
 async def create_schema(schema_name):
     try:
@@ -95,31 +106,40 @@ async def create_schema(schema_name):
     except Exception as e:
         print("An error occurred while creating scheama in the database:", e)
 
-def store_to_db(data, table_name, pilot):
-    try:
-        # Convert DataFrame to SQL table
-        pd.DataFrame(data).to_sql(table_name, engine, schema=pilot, if_exists='replace', index=False)
-        print("Data has been successfully stored in the database.")
-    except Exception as e:
-        print("An error occurred while storing data in the database:", e)
-
 async def find_schemas():
     with engine.connect() as connection:
         result = connection.execute(text("SELECT schema_name FROM information_schema.schemata"))
         schema_names = [row[0] for row in result.fetchall()]
         non_system_schemas = [schema for schema in schema_names if not schema.startswith("pg_") and schema != "information_schema"]
         return non_system_schemas  
+
+def get_endpoints(openapi_url):
+    response = requests.get(openapi_url)
+    openapi_spec = yaml.safe_load(response.text)  # Load YAML data
+
+    # Extract GET endpoints
+    endpoints = []
+
+    # Traverse through paths in the OpenAPI spec
+    for path, path_info in openapi_spec['paths'].items():
+        # Check if GET method exists for the path
+        if 'get' in path_info and path != '/':
+            # Extract endpoint URL
+            endpoint_url = path
+            endpoints.append(endpoint_url.lstrip("/"))  # Remove leading slash
     
+    return endpoints
+
 #  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Endpoints ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-@app.post('/print_schemas',tags=['Get Data'])
+@app.get('/print_schemas',tags=['Schemas'])
 async def print_schemas():
     return {"non_system_schemas": await find_schemas()}
 
-@app.post("/get_all_data", tags=['Get Data'])
-async def get_all_data( api_version: str  = '0.5',
-                        endpoint: str = 'efcomp',
-                        pilot: str = 'Pilot7',
-                        save: bool = True):
+@app.get("/get_data", tags=['Data'])
+async def get_data( api_version: str  = '0.5',
+                    endpoint: str = 'efcomp',
+                    pilot: str = 'Pilot7',
+                    save: bool = True):
 
     temp_headers = headers.copy()
     temp_headers['Forward-Id'] = temp_headers['Forward-Id'] + pilot
@@ -132,20 +152,30 @@ async def get_all_data( api_version: str  = '0.5',
     # Check if request was successful (status code 200)
     if response.status_code == 200:
         try:
+            print(f"Response size: {len(response.content)} bytes")
             data = response.json()  # Attempt to decode JSON
             if(save): 
                 await create_schema(pilot.lower()) # create schema if not exists - wait for schema to be created
                 store_to_db(data, endpoint, pilot.lower()) # store df to db
-            return data[:10]
+                print(data)
+            return {"message": f"{endpoint} from {pilot} has been added to db"} #data[:10]
         except ValueError:  # includes simplejson.decoder.JSONDecodeError
             print("Response content is not valid JSON")
             print(response.text)
+            return {"message": "Response content is not valid JSON",
+                    "response_text": response.text}
+        except Exception as e:
+            # Handle any exceptions that might occur during JSON parsing or other operations
+            print("Error:", e)
+            return {"Error:", e} # Return None or handle the error as appropriate
     else:
         print(f"Request failed with status code: {response.status_code}")
         print("Response text:", response.text)
+        return {"message": f"Request failed with status code: {response.status_code}"}
+
 
 # Endpoint to get a list of tables in the database
-@app.get("/get_tables", tags=["Get tables list"])
+@app.get("/get_tables", tags=["Tables"])
 async def get_tables():
 
     non_system_schemas = []
@@ -167,7 +197,7 @@ async def get_tables():
         raise HTTPException(status_code=500, detail=str(e))
 
 # Endpoint to get the first entries of a table 
-@app.get("/get_table", tags=["Get table dataframe"])
+@app.get("/get_table", tags=["Tables"])
 async def get_table_as_df(
     table_name: str = 'pilot7.efcomp',
     schema_name: str = 'pilot7',
